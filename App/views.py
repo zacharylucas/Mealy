ccc34d9babf37ccbc3ee06e11c8df9ef03449da1
Zweatherly36 <weatherly.15@osu.edu>
2018-02-13 21:44:11 -0500

    updates views.py

--- App/views.py
+++ App/views.py
@@ -2,27 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import User
 from django.template import loader
+from .Controllers import watsonQueries as wQ
 
 # Create your views here.
-def search(request):
-    parsedData = []
-    if request.method == 'POST':
-        username = request.POST.get('user')
-        #req = requests.get('https://api.github.com/users/' + username)
-        #jsonList = []
-        #jsonList.append(req.json())
-        userData = {}
-        userData['name'] = username
-        '''
-        for data in jsonList:
-            userData['name'] = data['name']
-            userData['blog'] = data['blog']
-            userData['email'] = data['email']
-            userData['public_gists'] = data['public_gists']
-            userData['public_repos'] = data['public_repos']
-            userData['avatar_url'] = data['avatar_url']
-            userData['followers'] = data['followers']
-            userData['following'] = data['following']
-        '''
-        parsedData.append(userData)
-    return render(request, 'app/search.html', {'data' : parsedData})
+def index(request):
+    context = {}
+    return render(request, 'app/index.html', context)
+
+def detail(request, page_id):
+    return HttpResponse("You're looking at app page %s." % page_id)
