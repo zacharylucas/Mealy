c38efd9d059d9412258feab9873ccc304f2fedc9
zacharylucas <lucas.598@buckeyemail.osu.edu>
2018-02-13 21:02:13 -0500

    merge templates

--- App/urls.py
+++ App/urls.py
@@ -3,8 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # ex: /app/
-    path('', views.index, name='index'),
-    # ex: /app/5/
-    path('<int:page_id>/', views.detail, name='detail'),
+    # ex: /search/
+    path('search/', views.search, name='search'),
 ]
--- App/views.py
+++ App/views.py
@@ -4,9 +4,25 @@ from .models import User
 from django.template import loader
 
 # Create your views here.
-def index(request):
-    context = {}
-    return render(request, 'app/index.html', context)
-
-def detail(request, page_id):
-    return HttpResponse("You're looking at app page %s." % page_id)
+def search(request):
+    parsedData = []
+    if request.method == 'POST':
+        username = request.POST.get('user')
+        #req = requests.get('https://api.github.com/users/' + username)
+        #jsonList = []
+        #jsonList.append(req.json())
+        userData = {}
+        userData['name'] = username
+        '''
+        for data in jsonList:
+            userData['name'] = data['name']
+            userData['blog'] = data['blog']
+            userData['email'] = data['email']
+            userData['public_gists'] = data['public_gists']
+            userData['public_repos'] = data['public_repos']
+            userData['avatar_url'] = data['avatar_url']
+            userData['followers'] = data['followers']
+            userData['following'] = data['following']
+        '''
+        parsedData.append(userData)
+    return render(request, 'app/search.html', {'data' : parsedData})
