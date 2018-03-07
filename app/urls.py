a176f713d3419d26c130c58ba46cb393645fe5c6
Sectionalized <dinsmorezack@gmail.com>
2018-03-07 11:28:58 -0500

    chatbot merge

--- app/urls.py
+++ app/urls.py
@@ -9,9 +9,4 @@ urlpatterns = [
 	path('conversation/', views.conversation, name='conversation'),
     path('preferenceSelection/', views.preferenceSelection, name='preferenceSelection'),
     path('chatbot/', views.chatbot, name = 'chatbot')
-<<<<<<< HEAD
-    path('preferenceSelection/', views.preferenceSelection, name='preferenceSelection')
-=======
-    path('chatbot/', views.chatbot, name = 'chatbot')
->>>>>>> 9dfe23cf147d4e2370b835541d5d507f87506d41
 ]
