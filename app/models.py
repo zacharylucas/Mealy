87dfc69170d46458b8724cf6542a8ac6e8ecc47f
zacharylucas <lucas.598@buckeyemail.osu.edu>
2018-03-27 16:56:59 -0400

    recreated database

--- app/models.py
+++ app/models.py
@@ -11,17 +11,21 @@ class Meal(models.Model):
     discoveryId = models.CharField(max_length=128)
     name = models.CharField(max_length=128)
     
+class Pref_Dict(models.Model):
+    name = models.CharField(max_length=32)
+    
+class Preference(models.Model):
+    prefDictId = models.ForeignKey(Pref_Dict, on_delete=models.CASCADE)
+    name = models.CharField(max_length=64)
+    
 class User_Meal(models.Model):
     userId = models.ForeignKey(User, on_delete=models.CASCADE)
     mealId = models.ForeignKey(Meal, on_delete=models.CASCADE)
     date = models.DateField()
     break_lunch_dinner = models.IntegerField()
     
-class Preference(models.Model):
-    name = models.CharField(max_length=64)
-    
 class User_Preference(models.Model):
     userId = models.ForeignKey(User, on_delete=models.CASCADE)
     preferenceId = models.ForeignKey(Preference, on_delete=models.CASCADE)
-    like_dislike = models.BooleanField()
+    like_neutral_dislike = models.IntegerField()
 
