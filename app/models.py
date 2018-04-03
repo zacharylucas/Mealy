fefd604aed226993e0471548364a9f8ff772f808
zacharylucas <lucas.598@buckeyemail.osu.edu>
2018-04-03 16:04:45 -0400

    models, db init

--- app/models.py
+++ app/models.py
@@ -7,6 +7,9 @@ class UserInfo(models.Model):
     height = models.IntegerField()
     gain_lose_maintain = models.IntegerField()
     cell = models.CharField(max_length=12,default='')
+    breakTime = models.TimeField(blank=True,null=True)
+    lunchTime = models.TimeField(blank=True,null=True)
+    dinnerTime = models.TimeField(blank=True,null=True)
     
 class Meal(models.Model):
     discoveryId = models.CharField(max_length=128)
