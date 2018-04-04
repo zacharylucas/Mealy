b5103def80d254623f779da380d0c7490704fc90
Zweatherly36 <weatherly.15@osu.edu>
2018-04-04 17:24:11 -0400

    changes preference selection to use Django Forms (this was a pain)

--- app/Controllers/Preference.py
+++ app/Controllers/Preference.py
@@ -1,69 +1,110 @@
 
-def createPreferences(request):
+def createPreferences(form):
     proteinDict = {
-        'chicken' : request.POST.get('radio-group-chicken'),
-        'steak' : request.POST.get('radio-group-steak'),
-        'beef' : request.POST.get('radio-group-beef'),
-        'pork' : request.POST.get('radio-group-pork'),
-        'eggs' : request.POST.get('radio-group-eggs'),
-        'beans' : request.POST.get('radio-group-beans'),
-        'tofu' : request.POST.get('radio-group-tofu')
+        'chicken' : form.__dict__['data']['chicken'],
+        'steak' : form.__dict__['data']['steak'],
+        'beef' : form.__dict__['data']['beef'],
+        'pork' : form.__dict__['data']['pork'],
+        'bacon' : form.__dict__['data']['bacon'],
+        'sausage' : form.__dict__['data']['sausage'],
+        'ham' : form.__dict__['data']['ham'],
+        'turkey' : form.__dict__['data']['turkey'],
+        'lamb' : form.__dict__['data']['lamb'],
+        'eggs' : form.__dict__['data']['eggs'],
+        'beans' : form.__dict__['data']['beans'],
+        'tofu' : form.__dict__['data']['tofu'],
+        'fish' : form.__dict__['data']['fish'],
+        'salmon' : form.__dict__['data']['salmon'],
+        'tilapia' : form.__dict__['data']['tilapia'],
+        'tuna' : form.__dict__['data']['tuna']
     }
     carbDict = {
-        'pasta' : request.POST.get('radio-group-pasta'),
-        'white-bread' : request.POST.get('radio-group-white-bread'),
-        'wheat-bread' : request.POST.get('radio-group-wheat-bread'),
-        'rice' : request.POST.get('radio-group-rice'),
-        'quinoa' : request.POST.get('radio-group-quinoa'),
-        'whole-grains' : request.POST.get('radio-group-whole-grains')
+        'pasta' : form.__dict__['data']['pasta'],
+        'bread' : form.__dict__['data']['wheat-bread'],
+        'rice' : form.__dict__['data']['rice'],
+        'quinoa' : form.__dict__['data']['quinoa'],
+        'whole-grain' : form.__dict__['data']['whole-grains'],
+        'whole-wheat' : form.__dict__['data']['whole-wheat'],
+        'tortilla' : form.__dict__['data']['tortillas'],
+        'oat' : form.__dict__['data']['oat'],
+        'cereal' : form.__dict__['data']['cereal'],
+        'pita' : form.__dict__['data']['pita']
     }
     fruitDict = {
-        'bananas' : request.POST.get('radio-group-bananas'),
-        'berries' : request.POST.get('radio-group-berries'),
-        'melons' : request.POST.get('radio-group-melons'),
-        'apples' : request.POST.get('radio-group-apples'),
-        'citruses' : request.POST.get('radio-group-citruses'),
-        'lemons' : request.POST.get('radio-group-lemons'),
-        'lime' : request.POST.get('radio-group-lime')
+        'banana' : form.__dict__['data']['banana'],
+        'strawberry' : form.__dict__['data']['strawberry'],
+        'raspberry' : form.__dict__['data']['raspberry'],
+        'blueberry' : form.__dict__['data']['blueberry'],
+        'cantelope' : form.__dict__['data']['cantelope'],
+        'pineapple' : form.__dict__['data']['pineapple'],
+        'coconut' : form.__dict__['data']['coconut'],
+        'grape' : form.__dict__['data']['grape'],
+        'mango' : form.__dict__['data']['mango'],
+        'pear' : form.__dict__['data']['pear'],
+        'plum' : form.__dict__['data']['plum'],
+        'peach' : form.__dict__['data']['peach'],
+        'watermelon' : form.__dict__['data']['watermelon'],
+        'apple' : form.__dict__['data']['apple'],
+        'orange' : form.__dict__['data']['orange'],
+        'lemon' : form.__dict__['data']['lemons'],
+        'lime' : form.__dict__['data']['lime']
     }
     vegetableDict = {
-        'carrots' : request.POST.get('radio-group-carrots'),
-        'squash' : request.POST.get('radio-group-squash'),
-        'cucumbers' : request.POST.get('radio-group-cucumbers'),
-        'peppers' : request.POST.get('radio-group-peppers'),
-        'onions' : request.POST.get('radio-group-onions'),
-        'celery' : request.POST.get('radio-group-celery'),
-        'leafy-greens' : request.POST.get('radio-group-leafy-greens')
+        'carrot' : form.__dict__['data']['carrot'],
+        'squash' : form.__dict__['data']['squash'],
+        'cucumber' : form.__dict__['data']['cucumber'],
+        'bell-pepper' : form.__dict__['data']['bell-pepper'],
+        'onion' : form.__dict__['data']['onions'],
+        'celery' : form.__dict__['data']['celery'],
+        'tomato' : form.__dict__['data']['tomato'],
+        'potato' : form.__dict__['data']['potato'],
+        'broccoli' : form.__dict__['data']['broccoli'],
+        'corn' : form.__dict__['data']['corn'],
+        'mushroom' : form.__dict__['data']['mushroom'],
+        'lettuce' : form.__dict__['data']['lettuce'],
+        'spinach' : form.__dict__['data']['spinach'],
+        'kale' : form.__dict__['data']['kale'],
+        'sweet-potato' : form.__dict__['data']['sweet-potato'],
+        'zucchini' : form.__dict__['data']['zucchini'],
+        'cucumber' : form.__dict__['data']['cucumber'],
+        'asparagus' : form.__dict__['data']['asparagus']
     }
     dairyDict = {
-        'milk' : request.POST.get('radio-group-milk'),
-        'yogurt' : request.POST.get('radio-group-yogurt'),
-        'cheese' : request.POST.get('radio-group-cheese'),
-        'skim-milk' : request.POST.get('radio-group-skim-milk')
+        'milk' : form.__dict__['data']['milk'],
+        'yogurt' : form.__dict__['data']['yogurt'],
+        'cheese' : form.__dict__['data']['cheese'],
+        'american-cheese' : form.__dict__['data']['american cheese'],
+        'mozzerella' : form.__dict__['data']['mozzerella'],
+        'provolone' : form.__dict__['data']['provolone'],
+        'swiss' : form.__dict__['data']['swiss'],
+        'goat-cheese' : form.__dict__['data']['goat-cheese'],
+        'blue-cheese' : form.__dict__['data']['blue-cheese'],
+        'skim-milk' : form.__dict__['data']['skim-milk'],
+        'butter' : form.__dict__['data']['butter']
     }
     spiceDict = {
-        'garlic' : request.POST.get('radio-group-garlic'),
-        'chili-powder' : request.POST.get('radio-group-chili-powder'),
-        'cayenne' : request.POST.get('radio-group-cayenne'),
-        'cumin' : request.POST.get('radio-group-cumin'),
-        'ginger' : request.POST.get('radio-group-ginger'),
-        'paprika' : request.POST.get('radio-group-paprika'),
-        'saffron' : request.POST.get('radio-group-saffron'),
+        'garlic' : form.__dict__['data']['garlic'],
+        'chili-powder' : form.__dict__['data']['chili-powder'],
+        'cayenne' : form.__dict__['data']['cayenne'],
+        'cumin' : form.__dict__['data']['cumin'],
+        'ginger' : form.__dict__['data']['ginger'],
+        'paprika' : form.__dict__['data']['paprika'],
+        'saffron' : form.__dict__['data']['saffron'],
+        'cinnamon' : form.__dict__['data']['cinammon'],
+        'curry-powder' : form.__dict__['data']['curry-powder'],
+        'tumeric' : form.__dict__['data']['tumeric'],
+        'ginger' : form.__dict__['data']['ginger']
     }
     herbDict = {
-        'oregano' : request.POST.get('radio-group-oregano'),
-        'cilantro' : request.POST.get('radio-group-cilantro'),
-        'thyme' : request.POST.get('radio-group-thyme'),
-        'basil' : request.POST.get('radio-group-basil'),
-        'parsley' : request.POST.get('radio-group-parsley'),
-        'rosemary' : request.POST.get('radio-group-rosemary'),
-        'mint' : request.POST.get('radio-group-mint'),
-    }
-    sweetDict = {
-        'cake' : request.POST.get('radio-group-cake'),
-        'pudding' : request.POST.get('radio-group-pudding'),
-        'pie' : request.POST.get('radio-group-pie'),
-        'ice-cream' : request.POST.get('radio-group-ice-cream'),
+        'oregano' : form.__dict__['data']['oregano'],
+        'cilantro' : form.__dict__['data']['cilantro'],
+        'thyme' : form.__dict__['data']['thyme'],
+        'basil' : form.__dict__['data']['basil'],
+        'parsley' : form.__dict__['data']['parsley'],
+        'rosemary' : form.__dict__['data']['rosemary'],
+        'mint' : form.__dict__['data']['mint'],
+        'sage' : form.__dict__['data']['sage'],
+        'bay-leaf' : form.__dict__['data']['bay-leaf']
     }
     prefDict = {
         'proteinDict' : proteinDict,
@@ -73,6 +114,5 @@ def createPreferences(request):
         'dairyDict' : dairyDict,
         'spiceDict' : spiceDict,
         'herbDict' : herbDict,
-        'sweetDict' : sweetDict,
     }
     return prefDict
