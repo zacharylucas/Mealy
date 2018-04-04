9241a946dc73bafd6fe737dee82d7193150dec9f
zacharylucas <lucas.598@buckeyemail.osu.edu>
2018-04-04 15:38:31 -0400

    new db for dicts

--- app/models.py
+++ app/models.py
@@ -6,21 +6,16 @@ class UserInfo(models.Model):
     weight = models.IntegerField()
     height = models.IntegerField()
     gain_lose_maintain = models.IntegerField()
-
-class Meal(models.Model):
-    discoveryId = models.CharField(max_length=128)
-    name = models.CharField(max_length=128)
-
-class User_Meal(models.Model):
+    cell = models.CharField(max_length=12,default='')
+    breakTime = models.TimeField(blank=True,null=True)
+    lunchTime = models.TimeField(blank=True,null=True)
+    dinnerTime = models.TimeField(blank=True,null=True)
+    prefDict = models.CharField(max_length=2048,default='')
+    mealDict = models.CharField(max_length=2048,default='')
+    
+class User_Meals(models.Model):
     userId = models.ForeignKey(User, on_delete=models.CASCADE)
-    mealId = models.ForeignKey(Meal, on_delete=models.CASCADE)
     date = models.DateField()
     break_lunch_dinner = models.IntegerField()
-
-class Preference(models.Model):
-    name = models.CharField(max_length=64)
-
-class User_Preference(models.Model):
-    userId = models.ForeignKey(User, on_delete=models.CASCADE)
-    preferenceId = models.ForeignKey(Preference, on_delete=models.CASCADE)
-    like_dislike = models.BooleanField()
+    discoveryId = models.CharField(max_length=128)
+    name = models.CharField(max_length=128)
